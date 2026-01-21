f68a92a42c3e198cc94ea7f929b2e35c1ccdfdc9
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-21 17:48:05 -0500

    Configure LibreWolf partially

--- apps/librewolf/home-manager.nix
+++ apps/librewolf/home-manager.nix
@@ -17,6 +17,22 @@
         ];
         Default = "Startpage";
       };
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = { # uBlock Origin
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = { # Bitwarden
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+          default_area = "menupanel";
+        };
+        "jid0-3GUEt1r69sQNSrca5p8kx9Ezc3U@jetpack" = { # Terms of Service; Didn't Read
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/terms-of-service-didnt-read/latest.xpi";
+          installation_mode = "force_installed";
+          default_area = "menupanel";
+        }
+      }
     };
     settings = {
       # Librewolf-specific settings
@@ -24,16 +40,18 @@
       "webgl.disabled" = false;
       "privacy.clearOnShutdown.history" = false;
       "privacy.clearOnShutdown.downloads" = false;
-      # Firefox settings, put them down below this line
+      # Firefox settings
+      "browser.theme.content-theme" = 0; # Dark theme!
+      "media.eme.enabled" = true; # Encrypted Media Extensions, or DRM-protected media
     } // lib.optionalAttrs (config.programs.plasma6.enable or false) { # If Plasma 6 is enabled, then include these in preferences
       "widget.use-xdg-desktop-portal.file-picker" = 1;
     };
     profiles = {
       "MainProfile" = {
-        
+        id = 0;
       };
       "School" = {
-
+        id = 1;
       };
     };
   };
