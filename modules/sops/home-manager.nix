d99745a28c28bd30c7a0fe6e9f997a5735877773
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-21 16:42:21 -0500

    Oops, again!

--- modules/sops/home-manager.nix
+++ modules/sops/home-manager.nix
@@ -4,6 +4,6 @@
     defaultSopsFile = ../../secrets/secrets.yaml; # Secrets encrypted with the public key
     defaultSopsFormat = "yaml";
     age.keyFile = "/home/${userName}/.config/sops/age/keys.txt";
-    sops.age.generateKey = true; # Generate keys.txt if it doesn't exist
+    age.generateKey = true; # Generate keys.txt if it doesn't exist
   };
 }
