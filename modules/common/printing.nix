634ef2a8c26e950b1773214665b932aa80a1e37f
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-21 16:33:43 -0500

    Fix printing module

--- modules/common/printing.nix
+++ modules/common/printing.nix
@@ -1,18 +1,20 @@
 { pkgs, ... }:
 
 {
-  avahi = {
-    # Daemon that allows for automatic discovery of services on local networks
-    # In this case, it's a prerequisite for automatically finding printers on the network
-    enable = true;
-    nssmdns4 = true;
-    # mDNS Name Server Switch plugin, allows for applications to resolve devices on the .local domain
-  }; # By enabling Avahi, we now search on UDP port 5353 for printers
-  printing = {
-    enable = true;
-    drivers = with pkgs; [
-    cups-filters # Includes "filters", which are used to convert data into a printer-receivable format
-    cups-browsed # A helper daemon to Avahi that helps with printer discovery
-    ];
+  services = {
+    avahi = {
+      # Daemon that allows for automatic discovery of services on local networks
+      # In this case, it's a prerequisite for automatically finding printers on the network
+      enable = true;
+      nssmdns4 = true;
+      # mDNS Name Server Switch plugin, allows for applications to resolve devices on the .local domain
+    }; # By enabling Avahi, we now search on UDP port 5353 for printers
+    printing = {
+      enable = true;
+      drivers = with pkgs; [
+      cups-filters # Includes "filters", which are used to convert data into a printer-receivable format
+      cups-browsed # A helper daemon to Avahi that helps with printer discovery
+      ];
+    };
   };
 }
