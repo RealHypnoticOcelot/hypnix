a599f438686fec4b28491e877bcd3f253b09e270
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-21 17:16:25 -0500

    I must brush up on Attribute Sets!

--- apps/vesktop/home-manager.nix
+++ apps/vesktop/home-manager.nix
@@ -7,7 +7,7 @@
       discordBranch = "canary";
     };
     vencord.themes = {
-      Pesterchum = "./themes/pesterchum.css"
+      Pesterchum = "./themes/pesterchum.css";
     };
     vencord.settings = {
       plugins = {
@@ -69,7 +69,7 @@
         VoiceMessages.enabled = true;
         YoutubeAdblock.enabled = true;
       };
-      enabledThemes = [ "Pesterchum" ];
+      enabledThemes = [ "pesterchum.css" ];
     };
   };
 }
