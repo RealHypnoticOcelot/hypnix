945e0afa1e51e538caff3c813bfdb8886852388a
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-21 18:28:13 -0500

    Then again..

--- hosts/mkhost.nix
+++ hosts/mkhost.nix
@@ -85,7 +85,7 @@ lib.nixosSystem {
   ]
   ++ systemModules
   ++ extraModules
-  ++ (if profiles ? impermanence then [ # If impermanence is one of the profiles, import the module that handles it specially
+  ++ (if profiles ? "impermanence" then [ # If impermanence is one of the profiles, import the module that handles it specially
     (import ../modules/impermanence/mkpersist.nix {
       inherit inputs profiles extraPersist extraHomeManagerPersist;
     })
