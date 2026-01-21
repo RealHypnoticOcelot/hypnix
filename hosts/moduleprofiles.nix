024826a423f8cae0de6d7a7ac1909621038b47e4
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-21 16:03:19 -0500

    Fix moduleprofiles x2

--- hosts/moduleprofiles.nix
+++ hosts/moduleprofiles.nix
@@ -1,88 +1,86 @@
 { inputs, systemDisk, ... }:
 
 {
-  moduleProfiles = {
-    sops = {
-      system = [
-        inputs.sops-nix.nixosModules.sops
-        ../modules/sops
-      ];
-      home-manager = [
-        inputs.sops-nix.homeManagerModules.sops
-        ../modules/sops/home-manager.nix
-      ];
-    };
-    disko = {
-      system = [
-        inputs.disko.nixosModules.disko
-      ];
-    };
-    impermanence = {
-      # This is handled specially in mkHost{}
-      system = [
-        inputs.impermanence.nixosModules.impermanence
-      ];
-    };
-    stylix = {
-      system = [
-        inputs.stylix.nixosModules.stylix
-        ../modules/stylix
-      ];
-      home-manager = [
-        inputs.stylix.homeModules.stylix
-        ../modules/stylix/home-manager.nix
-      ];
-    };
-    ucodenix = {
-      system = [
-        inputs.ucodenix.nixosModules.default
-        ../modules/ucodenix
-      ];
-    };
-    librewolf = {
-      home-manager = [
-        ../apps/librewolf/home-manager.nix
-      ];
-    };
-    vesktop = {
-      home-manager = [
-        ../apps/vesktop/home-manager.nix
-      ];
-    };
-    printing = {
-      system = [
-        ../modules/common/printing.nix
-      ];
-    };
-    networking-networkmanager-iwdbackend = {
-      system = [
-        ../modules/common/networking/networkmanager-iwdbackend.nix
-      ];
-    };
-    pipewire = {
-      system = [
-        ../modules/common/pipewire.nix
-      ];
-    };
-    grub = {
-      system = [
-        ../modules/common/bootloader/grub.nix
-      ];
-    };
-    systemd-boot = {
-      system = [
-        ../modules/common/bootloader/systemd-boot.nix
-      ];
-    };
-    steam = {
-      system = [
-        ../apps/steam
-      ];
-    };
-    obs = {
-      home-manager = [
-        ../apps/obs
-      ];
-    };
+  sops = {
+    system = [
+      inputs.sops-nix.nixosModules.sops
+      ../modules/sops
+    ];
+    home-manager = [
+      inputs.sops-nix.homeManagerModules.sops
+      ../modules/sops/home-manager.nix
+    ];
+  };
+  disko = {
+    system = [
+      inputs.disko.nixosModules.disko
+    ];
+  };
+  impermanence = {
+    # This is handled specially in mkHost{}
+    system = [
+      inputs.impermanence.nixosModules.impermanence
+    ];
+  };
+  stylix = {
+    system = [
+      inputs.stylix.nixosModules.stylix
+      ../modules/stylix
+    ];
+    home-manager = [
+      inputs.stylix.homeModules.stylix
+      ../modules/stylix/home-manager.nix
+    ];
+  };
+  ucodenix = {
+    system = [
+      inputs.ucodenix.nixosModules.default
+      ../modules/ucodenix
+    ];
+  };
+  librewolf = {
+    home-manager = [
+      ../apps/librewolf/home-manager.nix
+    ];
+  };
+  vesktop = {
+    home-manager = [
+      ../apps/vesktop/home-manager.nix
+    ];
+  };
+  printing = {
+    system = [
+      ../modules/common/printing.nix
+    ];
+  };
+  networking-networkmanager-iwdbackend = {
+    system = [
+      ../modules/common/networking/networkmanager-iwdbackend.nix
+    ];
+  };
+  pipewire = {
+    system = [
+      ../modules/common/pipewire.nix
+    ];
+  };
+  grub = {
+    system = [
+      ../modules/common/bootloader/grub.nix
+    ];
+  };
+  systemd-boot = {
+    system = [
+      ../modules/common/bootloader/systemd-boot.nix
+    ];
+  };
+  steam = {
+    system = [
+      ../apps/steam
+    ];
+  };
+  obs = {
+    home-manager = [
+      ../apps/obs
+    ];
   };
 }
