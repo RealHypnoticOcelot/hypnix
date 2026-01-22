f872a529b4e9ba5b474d99e5d8f1489c9e32628e
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-22 17:53:03 -0500

    impermanence testing!

--- modules/impermanence/modulepersist.nix
+++ modules/impermanence/modulepersist.nix
@@ -1,15 +1,13 @@
 {
-  moduleProfiles = {
-    sops = {
-      system = [
-        "/etc/ssh" # Where the computer's(not user's) SSH keypairs live, this is necessary to ensure SOPS works as intended
-      ];
-    };
-    networking-networkmanager-iwdbackend = {
-      system = [
-        "/var/lib/iwd" # "iNet wireless daemon", daemon to improve connection speeds
-        "/var/lib/NetworkManager" # Allows for adding and configuring networks
-      ];
-    };
+  sops = {
+    system = [
+      "/etc/ssh" # Where the computer's(not user's) SSH keypairs live, this is necessary to ensure SOPS works as intended
+    ];
+  };
+  networking-networkmanager-iwdbackend = {
+    system = [
+      "/var/lib/iwd" # "iNet wireless daemon", daemon to improve connection speeds
+      "/var/lib/NetworkManager" # Allows for adding and configuring networks
+    ];
   };
 }
