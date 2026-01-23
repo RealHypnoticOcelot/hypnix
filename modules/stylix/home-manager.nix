825d5045d16ae6a489caf9146124fe5fb802bcfb
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-23 18:17:59 -0500

    maybe a little silly, let's see

--- modules/stylix/home-manager.nix
+++ modules/stylix/home-manager.nix
@@ -4,8 +4,9 @@
     autoEnable = false;
     targets = {
       hyprland = {
-        enable = true;
-        colors.enable = false;
+        # enable = true;
+        # colors.enable = false;
+        hyprpaper.enable = true;
       };
     };
     image = ./wallpapers/AliceGameUI.titlePlayerMenu.titlePlayerMenu_I47.png;
