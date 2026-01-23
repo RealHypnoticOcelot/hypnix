7cda9f9a5087e150ab44a0ca76f1cc0be7af5415
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-22 22:26:22 -0500

    ?

--- modules/common/window-system/display-manager/sddm-wayland.nix
+++ modules/common/window-system/display-manager/sddm-wayland.nix
@@ -1,4 +1,5 @@
 {
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.settings.General.DisplayServer = "wayland";
 }
