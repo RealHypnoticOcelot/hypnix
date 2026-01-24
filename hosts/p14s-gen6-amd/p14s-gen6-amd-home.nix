ef53a846aa2fe7d124f251fa03e1bbf5fbb4d364
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-23 21:29:03 -0500

    oops!

--- hosts/p14s-gen6-amd/p14s-gen6-amd-home.nix
+++ hosts/p14s-gen6-amd/p14s-gen6-amd-home.nix
@@ -12,7 +12,7 @@
   wayland.windowManager.hyprland = {
     settings = {
       binde = [ # Bind something that'll repeat if you hold it
-        ", xf86audiomicmute, exec, brightnessctl -d 'platform::micmute' s $(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -q MUTED && echo 0 || echo 1)"
+        ", xf86audiomicmute, exec, brightnessctl -d 'platform::micmute' s $(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -q MUTED && echo 0 || echo 1)"
         # Toggle the microphone LED to match the state of microphone mute
       ];
     };
