4e9c904778e28b49877fa9585a75d3acd454ff20
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-24 15:13:00 -0500

    fix wallpaper setting

--- modules/stylix/home-manager.nix
+++ modules/stylix/home-manager.nix
@@ -8,6 +8,9 @@
         colors.enable = false;
         hyprpaper.enable = true; # Allow for automatic setting of the wallpaper
       };
+      kde = {
+        enable = true;
+      }
     };
     image = ./wallpapers/AliceGameUI.titlePlayerMenu.titlePlayerMenu_I47.png;
   };
