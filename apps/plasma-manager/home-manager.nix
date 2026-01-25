86abde6be35700a06ccb9c905f445913e4a9300a
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-25 13:10:21 -0500

    like this?

--- apps/plasma-manager/home-manager.nix
+++ apps/plasma-manager/home-manager.nix
@@ -4,61 +4,61 @@
     enable = true;
     overrideConfig = true; # Whether this is the only source of truth for configuration
     workspace.colorScheme = "BreezeDark";
-    panels = { # The taskbar at the bottom of the screen
-      widgets = [ # The items on the taskbar
-        {
-          floating = false;
-        }
-        {
-          name = "org.kde.plasma.kickoff";
-          config = {
-            General = {
-              icon = "nix-snowflake-white";
-              alphaSort = true;
+    panels = [
+      { # The taskbar at the bottom of the screen
+        floating = false;
+        widgets = [ # The items on the taskbar
+          {
+            name = "org.kde.plasma.kickoff";
+            config = {
+              General = {
+                icon = "nix-snowflake-white";
+                alphaSort = true;
+              };
             };
-          };
-        }
-        {
-          name = "org.kde.plasma.icontasks"; # The icons for applications that you pin
-          config = {
-            General = {
-              launchers = [
-                # These are a pain to find! I found them by pinning them manually,
-                # running `nano ~/.config/plasma-org.kde.plasma.desktop-appletsrc`,
-                #. and checking the general applet configuration for 
-                # `org.kde.plasma.icontasks`. Then, you have to change
-                # `file://nix/store/blahblahwhatever/yourapp.desktop` to `applications:yourapp.desktop`.
-                "applications:librewolf.desktop"
-                "applications:org.kde.dolphin.desktop"
-                "applications:steam.desktop"
+          }
+          {
+            name = "org.kde.plasma.icontasks"; # The icons for applications that you pin
+            config = {
+              General = {
+                launchers = [
+                  # These are a pain to find! I found them by pinning them manually,
+                  # running `nano ~/.config/plasma-org.kde.plasma.desktop-appletsrc`,
+                  #. and checking the general applet configuration for 
+                  # `org.kde.plasma.icontasks`. Then, you have to change
+                  # `file://nix/store/blahblahwhatever/yourapp.desktop` to `applications:yourapp.desktop`.
+                  "applications:librewolf.desktop"
+                  "applications:org.kde.dolphin.desktop"
+                  "applications:steam.desktop"
+                ];
+              };
+            };
+          }
+          {
+            name = "org.kde.plasma.marginsseparator";
+          }
+          {
+            systemTray.items = {
+              # We explicitly show bluetooth and battery
+              shown = [
+                "org.kde.plasma.battery"
+                "org.kde.plasma.bluetooth"
+              ];
+              # And explicitly hide networkmanagement and volume
+              hidden = [
+                "org.kde.plasma.networkmanagement"
+                "org.kde.plasma.volume"
               ];
             };
-          };
-        }
-        {
-          name = "org.kde.plasma.marginsseparator";
-        }
-        {
-          systemTray.items = {
-            # We explicitly show bluetooth and battery
-            shown = [
-              "org.kde.plasma.battery"
-              "org.kde.plasma.bluetooth"
-            ];
-            # And explicitly hide networkmanagement and volume
-            hidden = [
-              "org.kde.plasma.networkmanagement"
-              "org.kde.plasma.volume"
-            ];
-          };
-        }
-        {
-          digitalClock = {
-            calendar.firstDayOfWeek = "sunday";
-            time.format = "12h";
-          };
-        }
-      ];
-    };
+          }
+          {
+            digitalClock = {
+              calendar.firstDayOfWeek = "sunday";
+              time.format = "12h";
+            };
+          }
+        ];
+      }
+    ];
   };
 }
