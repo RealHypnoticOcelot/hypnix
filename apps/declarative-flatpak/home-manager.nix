a881e720f64dbe8da40caea74f5c9357fda34f37
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-25 21:47:50 -0500

    remove that

--- apps/declarative-flatpak/home-manager.nix
+++ apps/declarative-flatpak/home-manager.nix
@@ -3,7 +3,6 @@
 {
   services.flatpak = {
     enable = true;
-    update.onActivation = true; # Automatically update
     remotes = {
       "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
     };
