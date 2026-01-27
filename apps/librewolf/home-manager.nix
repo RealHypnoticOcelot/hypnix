4e8f1a19aa2008c19f87321fd09bb8f235cb3080
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-27 17:18:34 -0500

    startpage keeps serving me captchas with my custom preferences!

--- apps/librewolf/home-manager.nix
+++ apps/librewolf/home-manager.nix
@@ -7,10 +7,16 @@
       SearchEngines = {
         Add = [
           {
+            Name = "Mojeek";
+            Description = "Search the web using Mojeek";
+            URLTemplate = "https://www.mojeek.com/?sumt=0&theme=dark&sumb=0&q={searchTerms}";
+            Method = "POST";
+            IconURL = "https://www.mojeek.com/favicon.png";
+          }
+          {
             Name = "Startpage";
             Description = "Startpage Search";
-            URLTemplate = "https://www.startpage.com/sp/search?query={searchTerms}&prfe=d05529b22695d9b21ff0bfe3d59b0435c473b251863471092ba162b9a294373c050a20e9412164b63e1334d4b1fc2d4580f9085c3c08441b6809c72c78d039363453dbf0f832bdceebc83f234de3";
-            # REALLY long, but this contains user preferences! Now, when we search, theme and everything will always be set by default.
+            URLTemplate = "https://www.startpage.com/sp/search?query={searchTerms}";
             Method = "POST";
             IconURL = "https://www.startpage.com/favicon.ico";
             SuggestURLTemplate = "https://www.startpage.com/osuggestions?q={searchTerms}";
