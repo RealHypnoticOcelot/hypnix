b28a91bfe355dfb1b259b9d113a270e106884a88
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-28 18:58:29 -0500

    Does that work?

--- apps/merkuro/home-manager.nix
+++ apps/merkuro/home-manager.nix
@@ -1,10 +1,10 @@
-{ pkgs, config, ... }:
+{ pkgs, nixosConfig, ... }:
 
 {
   home.packages = with pkgs; [
     kdePackages.merkuro
   ]
-  ++ lib.optionals config.services.desktopManager.plasma6.enable [ # Packages needed for the Contacts widget on Plasma 6 to work properly
+  ++ lib.optionals nixosConfig.services.desktopManager.plasma6.enable [ # Packages needed for the Contacts widget on Plasma 6 to work properly
     kdePackages.kcontacts
     kdePackages.akonadi-contacts
   ];
