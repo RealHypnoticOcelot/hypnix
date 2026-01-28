4d240979f69b4249f77a86fa916a21f794cea725
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-28 17:19:19 -0500

    it should automatically add these in https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/kde/generated/dependencies.json, right?

--- apps/merkuro/home-manager.nix
+++ apps/merkuro/home-manager.nix
@@ -3,7 +3,5 @@
 {
   home.packages = with pkgs; [
     kdePackages.merkuro
-    kdePackages.kcontacts # Library for working with contacts that Merkuro uses for the contacts widget
-    kdePackages.kdepim-runtime # I think it needs this, but I can't tell!
   ];
 }
