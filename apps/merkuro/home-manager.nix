7b778a96f359bc43325e5d5742dc669c5aa21303
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-28 16:50:50 -0500

    let's find out!

--- apps/merkuro/home-manager.nix
+++ apps/merkuro/home-manager.nix
@@ -4,5 +4,6 @@
   home.packages = with pkgs; [
     kdePackages.merkuro
     kdePackages.kcontacts # Library for working with contacts that Merkuro uses for the contacts widget
+    kdePackages.kdepim-runtime # I think it needs this, but I can't tell!
   ];
 }
