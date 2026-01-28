efc3589d30a18dd72aafa346bfa235de19ee10a9
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-28 18:00:29 -0500

    i had to do some digging for this one!

--- apps/merkuro/home-manager.nix
+++ apps/merkuro/home-manager.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    kdePackages.merkuro
-  ];
+  # home.packages = with pkgs; [
+  #   kdePackages.merkuro
+  #   kdePackages.akonadi
+  #   kdePackages.kcontacts
+  # ];
+  programs.kde-pim = {
+    enable = true;
+    merkuro = true;
+  }
 }
