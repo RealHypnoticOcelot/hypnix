c055a80e2322327f2753ddafd605ac61b9c2d750
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-29 16:54:18 -0500

    wishful thinking!

--- hosts/p14s-gen6-amd/p14s-gen6-amd-home.nix
+++ hosts/p14s-gen6-amd/p14s-gen6-amd-home.nix
@@ -41,7 +41,7 @@
       # Funny business with escaping
     };
     shortcuts = {
-      "services/plasma-manager-commands.desktop".mute-microphone = "XF86AudioMicMute";
+      "services/plasma-manager-commands.desktop".mute-microphone = "Microphone Mute";
     };
   };
   programs.git = {
