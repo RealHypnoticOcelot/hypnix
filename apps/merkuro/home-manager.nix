6ec875927aa264ecc1e0b4a2b9ae35fe76a555d7
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-28 21:36:45 -0500

    may as well!

--- apps/merkuro/home-manager.nix
+++ apps/merkuro/home-manager.nix
@@ -4,8 +4,11 @@
   home.packages = with pkgs; [
     kdePackages.merkuro
   ]
-  ++ lib.optionals nixosConfig.services.desktopManager.plasma6.enable [ # Packages needed for the Contacts widget on Plasma 6 to work properly
+  ++ lib.optionals nixosConfig.services.desktopManager.plasma6.enable [
+    # Packages needed for the Contacts widget on Plasma 6 to work properly
     kdePackages.kcontacts
     kdePackages.akonadi-contacts
+    # Package for better integration into Plasma(like adding Merkuro calendars to the Digital Clock widget)
+    kdePackages.kdepim-addons
   ];
 }
