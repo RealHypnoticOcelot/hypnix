1b483344f026504a2c85edf8a1c605771e24906b
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-30 10:33:59 -0500

    describe reasons for kernel params on p14s

--- hosts/p14s-gen6-amd/p14s-gen6-amd.nix
+++ hosts/p14s-gen6-amd/p14s-gen6-amd.nix
@@ -10,8 +10,12 @@
 }:
 {
   boot.kernelParams = [
-    "microcode.amd_sha_check=off" # Disable microcode checksum verification so that ucodenix can do its thing
+    "microcode.amd_sha_check=off" # Disable microcode checksum verification so that ucodenix can update microcode properly
     "iommu=soft"
+    # This is enabled because of "snd_acp_pci 0000:c4:00.5: AMD-Vi: Event logged [IO_PAGE_FAULT domain=0x0018 address=0x98e75c5ea32c040 flags=0x0030]"
+    # Showing up in journalctl repeatedly. See https://bbs.archlinux.org/viewtopic.php?id=168467
+    # Fixes audio randomly cutting out briefly by using a software implementation of IOMMU(memory management unit) instead of
+    # the available hardware implementation. Prevents PCI passthrough for VMs, if that's something you're into!
   ];
 
   environment.systemPackages = with pkgs; [
