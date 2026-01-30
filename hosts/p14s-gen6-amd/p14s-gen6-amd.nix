480eb67d5f2687a354e4d88dc5ae65e8d7a99a4d
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-01-30 18:57:54 -0500

    turns out, this does nothing and doesn't pertain to my issue!

--- hosts/p14s-gen6-amd/p14s-gen6-amd.nix
+++ hosts/p14s-gen6-amd/p14s-gen6-amd.nix
@@ -11,11 +11,6 @@
 {
   boot.kernelParams = [
     "microcode.amd_sha_check=off" # Disable microcode checksum verification so that ucodenix can update microcode properly
-    "iommu=soft"
-    # This is enabled because of "snd_acp_pci 0000:c4:00.5: AMD-Vi: Event logged [IO_PAGE_FAULT domain=0x0018 address=0x98e75c5ea32c040 flags=0x0030]"
-    # showing up in journalctl repeatedly. See https://bbs.archlinux.org/viewtopic.php?id=168467
-    # Uses a software implementation of IOMMU(memory management unit) instead of the available hardware implementation.
-    # Prevents PCI passthrough for VMs, if that's something you're into!
   ];
 
   environment.systemPackages = with pkgs; [
