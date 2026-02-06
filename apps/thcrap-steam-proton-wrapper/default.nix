fed48eec0572763119b1ed334f1fe05dff94ae00
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-02-06 13:53:48 -0500

    clean it up

--- apps/thcrap-steam-proton-wrapper/default.nix
+++ apps/thcrap-steam-proton-wrapper/default.nix
@@ -1,8 +1,7 @@
 { pkgs, config, lib, ... }:
 
 {
-  # This is a system module so that we can do things like open ports for things like remote play.
   environment.systemPackages = with pkgs; [
-      thcrap-steam-proton-wrapper
+    thcrap-steam-proton-wrapper
   ];
 }
