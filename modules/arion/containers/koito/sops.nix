7dfcd64888a54f95ea9c26988ab70c440cdc4d7e
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-02-14 18:48:42 -0500

    oops!

--- modules/arion/containers/koito/sops.nix
+++ modules/arion/containers/koito/sops.nix
@@ -19,7 +19,7 @@ in
     );
     templates.${profileName}.content = ''
       KOITO_ALLOWED_HOSTS = ${config.sops.placeholder."${profileName}-allowed_hosts"}
-      KOITO_DATABASE_URL = postgres://postgres:${config.sops.placeholder."${profileName}-db_password"}@${profileName}_db:5432/koitodb";
+      KOITO_DATABASE_URL = postgres://postgres:${config.sops.placeholder."${profileName}-db_password"}@${profileName}_db:5432/koitodb
     '';
     templates."${profileName}_db".content = ''
       POSTGRES_PASSWORD = ${config.sops.placeholder."${profileName}-db_password"}
