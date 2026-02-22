e13ada1a6aec7ed0f85c6a822a779fc526e0e27f
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-02-21 23:03:17 -0500

    don't manage declaratively

--- apps/gallery-dl/home-manager.nix
+++ apps/gallery-dl/home-manager.nix
@@ -1,8 +1,5 @@
 {
-  programs.gallery-dl = {
-    enable = true;
-    settings = {
-      extractor.base-directory = "~/Archive";
-    };
-  };
+  home.packages = with pkgs; [
+    gallery-dl
+  ];
 }
