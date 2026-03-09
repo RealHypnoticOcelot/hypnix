160839f96003c62aed22d87a0e614bd6cca45cd6
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-03-09 13:11:03 -0400

    make anki work

--- apps/anki/home-manager.nix
+++ apps/anki/home-manager.nix
@@ -1,10 +1,7 @@
+{ pkgs, ... }:
+
 {
   programs.anki = {
     enable = true; # required for the default Hyprland config
-    addons = [
-      (with pkgs.ankiAddons; [
-        # put Anki Addons Here
-      ])
-    ]
   };
 }
