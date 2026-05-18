9aeeb4e9124c9662d8a07f57a5ec70c89ca060d6
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-05-18 14:05:52 -0400

    update home manager, update flake.lock since i guess the issue lies elsewhere

--- apps/vscodium/home-manager.nix
+++ apps/vscodium/home-manager.nix
@@ -7,6 +7,7 @@
       jnoortheen.nix-ide
       svelte.svelte-vscode
       platformio.platformio-vscode-ide
+      ms-vscode.cpptools # Required for PlatformIO IDE
     ];
   };
   home.packages = with pkgs; [
