e0fd2bfd5e0da7b3263287e644c6e4dc9a9bc2cf
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-06-01 20:16:19 -0400

    add winboat, update flake.lock

--- apps/winboat/default.nix
+++ apps/winboat/default.nix
@@ -1,8 +1,8 @@
 { pkgs, config, lib, ... }:
 
 {
+  virtualization.docker.enable = true;
   environment.systemPackages = with pkgs; [
-    kdePackages.dolphin
-    kdePackages.qtsvg
+    winboat
   ];
 }
