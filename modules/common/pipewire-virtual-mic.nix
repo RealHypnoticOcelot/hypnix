aa829b0a25ca5a896a02ca30bb525e1cbe5862a7
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2026-07-18 13:21:58 -0400

    rework

--- modules/common/pipewire-virtual-mic.nix
+++ modules/common/pipewire-virtual-mic.nix
@@ -1,26 +1,28 @@
 {
   services = {
-    pipewire.extraConfig.pipewire = {
-      "11-virtual-devices" = {
-        "context.modules" = [
-          {
-            name = "libpipewire-module-loopback";
-            args = {
-              "node.description" = "TTS Virtual Microphone";
-              "capture.props" = {
-                "node.name" = "tts_sink";
-                "node.description" = "TTS Sink";
-                "media.class" = "Audio/Sink";
+    pipewire.extraConfig = {
+      pipewire = {
+        "11-virtual-devices" = {
+          "context.modules" = [
+            {
+              name = "libpipewire-module-loopback";
+              args = {
+                "node.description" = "TTS Virtual Microphone";
+                "capture.props" = {
+                  "node.name" = "tts_sink";
+                  "node.description" = "TTS Sink";
+                  "media.class" = "Audio/Sink";
+                };
+                "playback.props" = {
+                  "node.name" = "tts_source";
+                  "node.description" = "TTS Source";
+                  "media.class" = "Audio/Source";
+                };
+                "audio.position" = [ "FL" "FR" ]; # Required, for some reason
               };
-              "playback.props" = {
-                "node.name" = "tts_source";
-                "node.description" = "TTS Source";
-                "media.class" = "Audio/Source";
-              };
-              "audio.position" = [ "FL" "FR" ]; # Required, for some reason
-            };
-          }
-        ];
+            }
+          ];
+        };
       };
     };
   };
